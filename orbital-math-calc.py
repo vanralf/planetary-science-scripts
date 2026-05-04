a425a5db3da449f3a7187dab86d03a7fa8e8ff81
Van Ralf Aguas <83374502+vanralf@users.noreply.github.com>
2026-05-04 13:10:40 -0700

    Fix string formatting for output messages

--- orbital-math-calc.py
+++ orbital-math-calc.py
@@ -12,18 +12,18 @@
 choice = input("Press P to calculate Period. Press a to calculate Distance")
 
 # Calculate Period given AU (a)
-if choice == 'P'
+if choice == 'P':
   a_str = input("Enter distance (a) in Astronomical Units (AU)")
   a = float(a_str)
   P = (a ** 3) ** 0.5
-  print("The Orbital Period is {P} Earth years.")
+  print(f"The Orbital Period is {P} Earth years.")
 
 # Calculate AU given Period
-elif choice == 'a'
+elif choice == 'a':
   p_str = input("Enter period (P) in years")
   p = float(p_str)
   a = (P ** 2) ** (1/3)
-  print("The Distance is {a} AU")
+  print(f"The Distance is {a} AU")
 
 else:
   print("Invalid choice. Run the script to try again")
